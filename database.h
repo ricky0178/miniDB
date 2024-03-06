8aa3149c38ef453824b19ecd1728471c721e470f
Ricky <riccardo.zucchelli@gmail.com>
2024-03-06 14:27:56 +0100

    first commit

---
+++ database.h
@@ -0,0 +1,44 @@
+
+// This represent a record in the only schema of this database
+enum Type {STR, INT};
+
+typedef struct Persona {
+    char name[20];
+    char surname[50];
+    char address[100];
+    int age;
+} Persona;
+
+// This is a node of an index that hold a string
+typedef struct Node {
+    Type type;
+    union{
+        int integer;
+        char* string;
+    } key;
+    Persona * val; 
+    struct Node * left;
+    struct Node * right;
+} Node;
+
+// A database hold a set of records and a set of indexes
+typedef struct {
+    Node * name;
+    Node * surname;
+    Node * address;
+    Node * age;
+} Database;
+
+// TODO implement the following methods
+// The method return a Persona or NULL 
+
+
+void insert(Database * database, Persona * persona);
+Persona* findByName(Database * database, char * name);
+Persona* findBySurname(Database * database, char * surname);
+Persona* findByAddress(Database * database, char * address);
+Persona* findByAge(Database * database, int age);
+
+//Custom
+Node* mkNode(Persona* persona, char* str, int num, Type type);
+void destroyDB(Database* database);
